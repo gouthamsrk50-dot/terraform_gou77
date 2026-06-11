92dd3757801ccb5c55cc9990271fb800b476be41
gouthamsrk50-dot <gouthamsrk50@gmail.com>
2026-06-11 14:18:12 +0530

    Added Terraform practice project

---
+++ any.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "demo" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-Practice"
+  }
+}
